6125e22fd055504fb52a9ae3d14b3beeffa20844
AMAIISDOM\mali <mohammadhussain.ali@amadeus.com>
2026-08-18 17:46:58 +0200

    feat: add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://mcp.supabase.com/mcp?project_ref=dzrpnyxyxhtvowgcvoco',
+  supabaseAnonKey: 'sb_publishable_mXKnp8ydVIkutr02dVUFxw_TvcVhFU4'
+};
